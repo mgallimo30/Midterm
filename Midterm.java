045f800208474876db028e94685b0b6d55aaa523
mgallimo30 <33467928+mgallimo30@users.noreply.github.com>
2017-11-15 22:21:25 -0500

    Update Midterm.java
    I added a new class "getfile" which brings in the test file and stores line one so far

--- Midterm.java
+++ Midterm.java
@@ -1,21 +1,66 @@
 import java.util.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class Midterm {
 	private static String[] team;
-	private static Double score; 
+	private static Double maximumScoreAllowed; 
 	
 	public static void main(String[] args) {
 		
-			String[] Gender = null; //Assumes this will hold "men" or "women"
+			getFile();
+			String[] Gender = {"men"}; //Assumes this will hold "men" or "women"
 			gender(Gender);
 			Double[] team2 = {0.00, 1.00, 2.00, 3.00, 4.00 }; //Assumes this will hold the teams personality type and number if members 
-			try {
+/*			try {
 				output();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
+			}*/
+	}
+
+	private static void getFile() {
+		
+		System.out.print("Enter input file: ");
+		Scanner in = new Scanner(System.in);
+		String input = in.next();
+		
+        // The name of the file to open.
+        String fileName = input + ".txt";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+/*            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }   */
+            
+           maximumScoreAllowed =  Double.parseDouble(line = bufferedReader.readLine());
+            
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+        }
 	}
 
 	private static int gender(String[] gender) {
@@ -54,19 +99,22 @@ public class Midterm {
 		
 	}
 		
-	public static void output() throws IOException {
+/*	public static void output() throws IOException {
 		
 		
 	System.out.print("Enter input file: ");
-	 FileInputStream in = null;
+	Scanner in = new Scanner(System.in);
+	String input = in.next();
+	
+	 FileInputStream in2 = null;
      FileOutputStream out = null;
 
       try {
-         in = new FileInputStream("input.txt");
+       //  in = new FileInputStream("input.txt");
          out = new FileOutputStream("output.txt");
          
          int c;
-         while ((c = in.read()) != -1) {
+         while ((c = in2.read()) != -1) {
             out.write(c);
          }
       }finally {
@@ -87,4 +135,4 @@ public class Midterm {
       
       
 }
-}
+*/}
